f2580168e2d34b22b3b1446380d69ec8f7c04a30
Ronando Long & Clinton Herrick <student@epicodus.com>
2015-08-12 09:54:17 -0700

    Add age and if American specs, both pass.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,8 +1,28 @@
 class Vehicle
   @@vehicles = []
 
+  define_method(:initialize) do |make, model, year|
+    @make = make
+    @model = model
+    @year = year
+
+  end
+
+
   define_singleton_method(:all) do
       @@vehicles
+  end
+
+  define_method(:age) do
+    current_year = Time.new().year
+    current_year.-(@year)
+  end
+
+  define_method(:worth_buying?) do
+    american_cars = ["Chrysler", "Ford", "GM"]
+    american = american_cars.include?(@make)
+    new_enough = self.age.<=(15)
+    american.&(new_enough)
   end
 
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -9,4 +9,28 @@ describe('Vehicle') do
     end
   end
 
+  describe('#age') do
+    it('determin the age of the vehicle') do
+       test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+       expect(test_vehicle.age()).to(eq(15))
+    end
+  end
+
+  describe('#worth_buying?') do
+    it('returns false if vehicle is NOT American and if it is less than 15 years old') do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      expect(test_vehicle.worth_buying?()).to(eq(false))
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
 end
