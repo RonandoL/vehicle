11c1d4c968d6989df9b5135a8e06946c810c47b9
Ronando Long & Clinton Herrick <student@epicodus.com>
2015-08-12 14:38:47 -0700

    reroute all file names and URLs

--- app.rb
+++ app.rb
@@ -8,18 +8,29 @@ get('/') do
   erb(:index)
 end
 
-get('/vehicles') do
-  erb(:see_vehicles)
+get('/vehicles') do   # @vehicles array displays vehicles on vehicle.erb page
+  @vehicles = Vehicle.all()
+  erb(:vehicles)
 end
 
-get('/add_vehicle') do
-   erb(:add_vehicle)
+get('/vehicles/new') do
+   erb(:vehicles_form)
 end
 
     get('/results') do
       erb(:results)
     end
 
-get('/vehicle_page') do
-   erb(:vehicle_page)
+get('/vehicle/:id') do
+  @vehicle = Vehicle.find(params.fetch("id").to_i())
+   erb(:vehicle)
+end
+
+post('/vehicles') do
+  make = params.fetch("make")
+  model = params.fetch("model")
+  year = params.fetch("year")
+  vehicle = Vehicle.new(make, model, year)
+  vehicle.save()
+  erb(:success)
 end
