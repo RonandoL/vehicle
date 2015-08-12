a2126f9f3feb6cf03924974ce9df07d8afaa93ab
Ronando Long & Clinton Herrick <student@epicodus.com>
2015-08-12 10:24:16 -0700

    Specs save, clear and make pass

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -9,7 +9,7 @@ class Vehicle
   end
 
 
-  define_singleton_method(:all) do
+  define_singleton_method(:all) do  # .all RETURNS the array
       @@vehicles
   end
 
@@ -23,6 +23,18 @@ class Vehicle
     american = american_cars.include?(@make)
     new_enough = self.age.<=(15)
     american.&(new_enough)
+  end
+
+  define_method(:save) do
+    @@vehicles.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@vehicles = []
+  end
+
+  define_method(:make) do
+    @make
   end
 
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -3,6 +3,11 @@ require('vehicle')
 
 describe('Vehicle') do
 
+  before()do
+    Vehicle.clear()
+  end
+
+
   describe('.all') do
     it('create an empty array') do
       expect(Vehicle.all()).to(eq([]))
@@ -23,7 +28,27 @@ describe('Vehicle') do
     end
   end
 
+  describe('#save') do
+    it('will save the vehicle to the array')do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      expect(Vehicle.all()).to(eq([test_vehicle]))
+    end
+  end
 
+  describe('.clear')do
+  it('will clear the vehicles array') do
+    expect(Vehicle.all()).to(eq([]))
+    end
+  end
+
+  describe('#make') do
+    it('returns make of the vehicle') do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.make()).to(eq("Toyota"))
+    end
+  end
 
 
 
