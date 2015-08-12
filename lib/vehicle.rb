453facbfd5ee49d65c42abaa6e85fd21c318c883
Ronando Long & Clinton Herrick <student@epicodus.com>
2015-08-12 10:58:40 -0700

    Will return vehicle from array of vehicles searching by id. All specs pass.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,7 +5,7 @@ class Vehicle
     @make = make
     @model = model
     @year = year
-
+    @id = @@vehicles.length().+(1)
   end
 
 
@@ -45,5 +45,18 @@ class Vehicle
     @year
   end
 
+  define_method(:id) do
+    @id
+  end
+
+  define_singleton_method(:find) do |identification|
+    found_vehicle = nil
+    @@vehicles.each() do |vehicle|
+      if vehicle.id().eql?(identification.to_i())
+        found_vehicle = vehicle
+      end
+    end
+    found_vehicle
+  end
 
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -62,14 +62,27 @@ describe('Vehicle') do
     it('returns year of the vehicle') do
       test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
       test_vehicle.save()
-      expect(test_vehicle.year()).to(eq(2000))      
+      expect(test_vehicle.year()).to(eq(2000))
     end
   end
 
+  describe('#id') do
+    it('assigns id to the vehicle') do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_vehicle.save()
+      expect(test_vehicle.id()).to(eq(1))
+    end
+  end
 
-
-
-
+   describe('.find') do
+     it('will find/return vehicle by id number from array of vehicles') do
+       test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+       test_vehicle.save()
+       test_vehicle2 = Vehicle.new("Ford", "F-150", 2012)
+       test_vehicle2.save()
+       expect(Vehicle.find(test_vehicle.id())).to(eq(test_vehicle))
+     end
+   end
 
 
 
